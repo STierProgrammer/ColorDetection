7d0f5237e096e6893c07d71ea4b109d75e758ad8
STierProgrammer <sabasajaia40@gmail.com>
2024-12-13 22:47:41 +0400

    Finished, fixed all problems

--- ColorDetection/main.cpp
+++ ColorDetection/main.cpp
@@ -3,33 +3,38 @@
 #include <opencv2/imgproc.hpp>
 #include <iostream>
 
-#define H_MIN 0
-#define S_MIN 110
-#define V_MIN 153
-
-#define H_MAX 19
-#define S_MAX 240
-#define V_MAX 255
-
 using std::string;
 using cv::Mat;
 using cv::Scalar;
 
 int main() {
+	int H_MIN = 0, S_MIN = 110, V_MIN = 153;
+	int H_MAX = 19, S_MAX = 240, V_MAX = 255;
+
 	Mat img = cv::imread("Resources/lambo.png");
 	Mat imgHSV, mask;
 
 	cv::cvtColor(img, imgHSV, cv::COLOR_BGR2HSV);
 
-	Scalar lower(H_MIN, S_MIN, V_MIN);
-	Scalar upper(H_MAX, S_MAX, V_MAX);
+	cv::namedWindow("Trackbars", (640, 200));
+	cv::createTrackbar("Hue Min", "Trackbars", &H_MIN, 179);
+	cv::createTrackbar("Hue Max", "Trackbars", &H_MAX, 179);
+	cv::createTrackbar("Sat Min", "Trackbars", &S_MIN, 255);
+	cv::createTrackbar("Sat Max", "Trackbars", &S_MAX, 255);
+	cv::createTrackbar("Val Min", "Trackbars", &V_MIN, 255);
+	cv::createTrackbar("Val Max", "Trackbars", &V_MAX, 255);
 
-	cv::inRange(imgHSV, lower, upper, mask);
+	while (true) {
+		Scalar lower(H_MIN, S_MIN, V_MIN);
+		Scalar upper(H_MAX, S_MAX, V_MAX);
 
-	cv::imshow("Image", img);
-	cv::imshow("Image HSV", imgHSV);
-	cv::imshow("Image Mask", mask);
-	cv::waitKey(0);
+		cv::inRange(imgHSV, lower, upper, mask);
+
+		cv::imshow("Image", img);
+		cv::imshow("Image HSV", imgHSV);
+		cv::imshow("Image Mask", mask);
+		cv::waitKey(1);
+	}
 
 	return 0;
 }
